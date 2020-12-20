10e9e35e1559aeffbd907dd27ca8433bc6762dea
soo5717 <koo5717@naver.com>
2020-12-20 16:59:13 +0900

    distribute_budget / plan / plan detail XML

---
+++ app/src/main/java/com/example/travellet/feature/plan/PlanDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.travellet.feature.plan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.travellet.R;
+
+public class PlanDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_detail);
+    }
+}
