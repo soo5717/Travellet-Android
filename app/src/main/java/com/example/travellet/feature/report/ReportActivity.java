e7022bafe7791fa6333db747a5c215c4b802e728
soo5717 <koo5717@naver.com>
2021-01-31 18:07:31 +0900

    report fragment

--- app/src/main/java/com/example/travellet/feature/report/ReportActivity.java
+++ app/src/main/java/com/example/travellet/feature/report/ReportActivity.java
@@ -1,17 +1,76 @@
 package com.example.travellet.feature.report;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.travellet.R;
+import com.example.travellet.databinding.ActivityReportBinding;
+import com.example.travellet.feature.util.BaseActivity;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-public class ReportActivity extends AppCompatActivity {
-    private final String[] TABS = {"DAILY", "CATEGORY"}; //탭 선언
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 수연 on 2021-01-31.
+ * Class: ReportActivity
+ * Description: 레포트(일별, 카테고리별) 페이지
+ */
+public class ReportActivity extends BaseActivity {
+    private ActivityReportBinding binding; //바인딩 선언
+
+    private final String[] TABS = {"daily", "category"}; //탭 선언
+    private List<Fragment> fragmentList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        initButton(); //버튼 클릭 이벤트 설정
+        initView(); //뷰 설정
+    }
+
+    @Override //Activity 뷰 바인딩
+    protected View getLayoutResource() {
+        binding = ActivityReportBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    //뷰 설정 : 탭, 뷰페이저, 프래그먼트
+    private void initView() {
+        //프래그먼트 리스트에 추가
+        fragmentList.add(ReportDailyFragment.newInstance(TABS[0], ""));
+        fragmentList.add(ReportDailyFragment.newInstance(TABS[1], ""));
+
+//        fragmentList.add(new ReportCategoryFragment());
+
+        //뷰페이저 어댑터 설정
+        binding.viewPager2.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                return fragmentList.get(position);
+            }
+
+            @Override
+            public int getItemCount() {
+                return fragmentList.size();
+            }
+        });
+
+        //탭과 뷰페이저 연결
+        new TabLayoutMediator(binding.tabs, binding.viewPager2, (tab, position) -> tab.setText(TABS[position])).attach();
+    }
+
+    //버튼 클릭 이벤트 설정
+    private void initButton() {
+        binding.buttonChange.setOnClickListener(v -> {
+            //버튼 이벤트 추후 추가 예정
+        });
     }
 }
